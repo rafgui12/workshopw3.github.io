af2115e40db269aa2934531f8844dcbaede92102
Rafael Angulo <rafgui12@gmail.com>
2021-12-12 22:41:41 -0500

    Task 1 and Task 2 are Finish

--- ColorGame.js
+++ ColorGame.js
@@ -1,45 +1,44 @@
 let COLORS_ARRAY = ['blue', 'cyan', 'gold', 'gray', 'green', 'magenta', 'orange', 'red', 'white', 'yellow'];
+var ColorSting = COLORS_ARRAY.toString().split(",").join(", ")
 let guess;
 let correct = false;
 const targetIndex = Math.floor(Math.random()*10);
 const target = COLORS_ARRAY[targetIndex];
+let numTries = 0; 
 
 function runGame() {
     console.log(target)
     do {
+        guess = prompt('I am thinking of one of these colors:\n\n'+ ColorSting +'\n\nWhat color am I thinking of?\n');
         if(guess === null){
             correct = true;
         }else{
-            guess = prompt('I am thinking of one of these colors:\n\n' + COLORS_ARRAY +
-                '\n\nWhat color am I thinking of?\n');
-            checkGuess (guess, target);
+            checkGuess(guess, target);
         }
       } while (correct === false);
 };
 
 function checkGuess(guess, target) {
 
-    console.log(COLORS_ARRAY.indexOf(guess))
-    console.log(COLORS_ARRAY.indexOf(target))
-
     if(COLORS_ARRAY.indexOf(guess) < 0){
 
-        alert("Color not recognized")
-        console.log("No esta en la lista");
-
-    }else if(COLORS_ARRAY.indexOf(guess) > COLORS_ARRAY.indexOf(target)){
-
-        console.log("Es esta abajo del color de la lista");
-        alert("guess is incorrect & is higher than target");
+        alert('Color not recognized')
+        numTries ++
 
     }else if(COLORS_ARRAY.indexOf(guess) < COLORS_ARRAY.indexOf(target)){
 
-        console.log("Es esta por arriba del color de la lista");
-        alert("guess is incorrect & is lower than target");
+        alert('guess is incorrect & is higher than target');
+        numTries ++
+
+    }else if(COLORS_ARRAY.indexOf(guess) > COLORS_ARRAY.indexOf(target)){
+
+        alert('guess is incorrect & is lower than target');
+        numTries ++
 
     }else{
 
-        alert("Congratulations!")
+        alert('Congratulations!, you guess in '+numTries+' tries')
+        document.body.style.background = guess.toString(); 
         correct = true;
 
     }
